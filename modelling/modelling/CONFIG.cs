67e5304c9c1fe93473f4d89dd6772d043d4b1306
andreikryuchkov <andrei_kryuchkov@bk.ru>
2014-09-09 14:27:13 +0400

    123468

---
+++ modelling/modelling/CONFIG.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace modelling
+{
+    public class config
+    {
+        public string dataBaseConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\model\modelling\modelling\App_Data\Database1.mdf;Integrated Security=True;User Instance=True";
+    }
+    
+}
