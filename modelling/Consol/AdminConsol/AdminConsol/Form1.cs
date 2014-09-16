70a0052be5629fc96d6308d2d27f4a13e664e1f3
Guuugol <abcd666@bk.ru>
2014-09-16 15:12:45 +0400

    Начало конслоли

---
+++ modelling/Consol/AdminConsol/AdminConsol/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Odbc;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace AdminConsol
+{
+    public partial class Form1 : Form
+    {
+
+        String dataBaseConnectionStringAdmin = "Data Source=SYSHCHIPANOV-H;" + "Initial Catalog=DATABASE1.MDF;" + "Integrated Security=True";
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Login.Text == "Admin")
+            {
+                SqlCommand q = new SqlCommand();
+                SqlConnection c = new SqlConnection(dataBaseConnectionStringAdmin);
+                c.Open();
+                q.Connection = c;
+                MessageBox.Show("Succesfully", "Connected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    //q.CommandText
+            }
+        }
+
+    }
+}
