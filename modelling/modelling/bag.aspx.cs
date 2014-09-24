427e110bb67f1ff94ddc304aff6e2f5bc81a623d
andreikryuchkov <andrei_kryuchkov@bk.ru>
2014-09-24 09:33:47 +0400

    merged

--- modelling/modelling/bag.aspx.cs
+++ modelling/modelling/bag.aspx.cs
@@ -11,7 +11,10 @@ namespace modelling
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if ((Request.QueryString["addItem"]) != null)
+            { 
+                
+            }
         }
     }
 }
--- modelling/modelling/catalog.aspx.cs
+++ modelling/modelling/catalog.aspx.cs
@@ -25,16 +25,16 @@ namespace modelling
             return result;
         }
 
-        public string formatOutputItem(string ImgURL, string Description, string Name, int Price)
+        public string formatOutputItem(string ImgURL, string Description, string Name, int Price, int ID)
         {
             string result = "";
-            result += "<div class=\"listItem\">" +
+            result += "<a href=\"bag.aspx?addItem=" + ID + "\">" + "<div class=\"listItem\">" +
                 "<h3>" + Name + "</h3>" +
               "<img class=\"listItemImg\" description=\"" + Description + "\" src=\"" + ImgURL + "\"/>" +
               "<p>" + Description + "</p>";
              result+= "<p style=\"Text-decoration:underline;\" >";
              result += Price + " Руб.</p>";
-             result+= "</div>";
+             result += "</div></a>";
             return result;
         }
 
@@ -46,7 +46,7 @@ namespace modelling
                 ctwwSQL.TextCommand = "select * from item where categoryID=" + Convert.ToInt32(Request.QueryString["category"]) + ";";
                 while (ctwwSQL.Reader.Read())
                 {
-                    categoryView.Text += formatOutputItem(ctwwSQL.GetStringValueReader(4), ctwwSQL.GetStringValueReader(5), ctwwSQL.GetStringValueReader(1), Convert.ToInt32(ctwwSQL.Reader.GetValue(2)));
+                    categoryView.Text += formatOutputItem(ctwwSQL.GetStringValueReader(4), ctwwSQL.GetStringValueReader(5), ctwwSQL.GetStringValueReader(1), Convert.ToInt32(ctwwSQL.Reader.GetValue(2)),Convert.ToInt32(ctwwSQL.Reader.GetValue(0)));
                 }
             }
             else
