a724a1c4ac1ca838fbbe4996fbd99adb4dd9088a
andreikryuchkov <andrei_kryuchkov@bk.ru>
2014-10-08 10:46:10 +0300

    new tables in base

---
+++ modelling/modelling/Order.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="Оформление заказа" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Order.aspx.cs" Inherits="modelling.Order" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:LoginView ID="Ordering" runat=server >
+        <AnonymousTemplate>
+            <p>Вы не авторизованы на сайте, необходимо войти в систему или пройти <a href="/registration.aspx" >регистрацию. </a> </p>
+        </AnonymousTemplate>
+        <LoggedInTemplate>
+            <p style="width:30%; display:inline-block;" > 
+                Введите адрес доставки. Если адрес не указан, доставка будет оформлена на адрес, указанный при регистрации.
+            </p>
+            <asp:TextBox ID="adressInput" runat=server  CssClass="adressInput"> </asp:TextBox>
+            <div class="buttonContainer"> <asp:button ID="orderDelivery" OnClick="orderDelivery_click" runat=server Text="Заказать!" /> </div>
+        </LoggedInTemplate>
+    </asp:LoginView>
+    <asp:Label runat=server ID="orderStatus" ForeColor=Red ></asp:Label>
+</asp:Content>
---
+++ modelling/modelling/Order.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+
+namespace modelling
+{
+    public partial class Order : System.Web.UI.Page
+    {
+        ClassToWorckWhithSQL ctwwSQL = new ClassToWorckWhithSQL();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var s = Session["itemsInBag"];
+            List<Item> items = (List<Item>)s;
+            if (items.Count == 0)
+            {
+                ((Button)(Ordering.FindControl("orderDelivery"))).Enabled = false;
+            }
+        }
+
+        protected void orderDelivery_click(object sender, EventArgs e)
+        {
+            var s = Session["itemsInBag"];
+            List<Item> items = (List<Item>)s;
+            int usrID = Convert.ToInt32(Session["userID"]);
+            ctwwSQL.TextCommand="SELECT Adress FROM usr WHERE ID="+usrID+";";
+            SqlDataReader reader=ctwwSQL.ExecuteReader;
+            reader.Read();
+            string adress;
+            if (((TextBox)(Ordering.FindControl("adressInput"))).Text.Length==0)
+                adress=Convert.ToString(reader.GetValue(0));
+            else 
+                adress=((TextBox)(Ordering.FindControl("adressInput"))).Text;
+            ctwwSQL.TextCommand = "INSERT INTO ord(usr,adress) values ("+usrID+",'"+adress+"');";
+            ctwwSQL.TextCommand = "SELECT max(ID) FROM ord WHERE usr=" + usrID + ";";
+            reader = ctwwSQL.ExecuteReader;
+            reader.Read();
+            int orderID = (int)reader.GetValue(0);
+            foreach (Item iter in items)
+            {
+                ctwwSQL.TextCommand = "INSERT INTO itemOrdered(ord,item) values (" + orderID + "," + iter.id + ");";
+            }
+            items.Clear();
+            Session["itemsInBag"] = items;
+            orderStatus.Text = "Ваш заказ принят!";
+        }
+    }
+}
